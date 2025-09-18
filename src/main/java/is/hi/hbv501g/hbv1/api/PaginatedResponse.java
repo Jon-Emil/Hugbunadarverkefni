8d876a5aa6c8459c6e97694c558fe2ed0522864d
Jon-Emil <jer8@hi.is>
2025-09-18 12:57:18 +0000

    gert pageination uppástungu

---
+++ src/main/java/is/hi/hbv501g/hbv1/api/PaginatedResponse.java
@@ -0,0 +1,67 @@
+package is.hi.hbv501g.hbv1.api;
+
+import java.util.List;
+
+/**
+ * This is a simple wrapper class for the final output of our API
+ * @param <T>
+ */
+
+public class PaginatedResponse<T> {
+    private int status;
+    private List<T> data;
+    private int total;
+    private int pageNr;
+    private int perPage;
+
+    public PaginatedResponse(int status, List<T> data, int pageNr, int perPage) {
+        this.status = status;
+        this.total = data.size();
+        this.pageNr = pageNr;
+        this.perPage = perPage;
+
+        int startIndex = Math.min((pageNr - 1) * perPage, data.size());
+        int endIndex = Math.min(startIndex + perPage, data.size());
+        this.data = data.subList(startIndex, endIndex);
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPageNr() {
+        return pageNr;
+    }
+
+    public void setPageNr(int pageNr) {
+        this.pageNr = pageNr;
+    }
+
+    public int getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
+    }
+}
--- src/main/java/is/hi/hbv501g/hbv1/controllers/HomeController.java
+++ src/main/java/is/hi/hbv501g/hbv1/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package is.hi.hbv501g.hbv1.controllers;
 
+import is.hi.hbv501g.hbv1.api.PaginatedResponse;
 import is.hi.hbv501g.hbv1.persistence.entities.Game;
 import is.hi.hbv501g.hbv1.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,13 +19,17 @@ public class HomeController {
         this.gameService = gameService;
     }
 
-    @RequestMapping("/")
-    public List<Game> homePage() {
+    @RequestMapping("/games")
+    public PaginatedResponse<Game> homePage(
+        @RequestParam(defaultValue = "1") int pageNr,
+        @RequestParam(defaultValue = "10") int perPage
+    ) {
         //Business logic
         //Call a method in a service class
         //Add some data to the model
         // we only return data not HTML templates
-        return gameService.findAll();
+        List<Game> allGames = gameService.findAll();
+        return new PaginatedResponse<Game>(200, allGames, pageNr,perPage);
     }
 
 
