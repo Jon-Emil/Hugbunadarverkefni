ac63f9331249ff615548c828533eaa989fcf478f
blaakornblom <yih2@hi.is>
2025-09-26 18:29:18 +0000

    minor fixes of id property of Admin

--- src/main/java/is/hi/hbv501g/hbv1/persistence/entities/Admin.java
+++ src/main/java/is/hi/hbv501g/hbv1/persistence/entities/Admin.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String email;
     private String hashed_password;
