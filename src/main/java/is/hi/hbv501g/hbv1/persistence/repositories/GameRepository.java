752996f5d271e6095a54badfd0a4759be72aa1d8
Jon-Emil <jer8@hi.is>
2025-10-03 19:01:29 +0000

    gerði search endapunktinn

--- src/main/java/is/hi/hbv501g/hbv1/persistence/repositories/GameRepository.java
+++ src/main/java/is/hi/hbv501g/hbv1/persistence/repositories/GameRepository.java
@@ -2,10 +2,11 @@ package is.hi.hbv501g.hbv1.persistence.repositories;
 
 import is.hi.hbv501g.hbv1.persistence.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
     Game save(Game game);
     List<Game> findByTitle(String title);
     Game findById(long id);
