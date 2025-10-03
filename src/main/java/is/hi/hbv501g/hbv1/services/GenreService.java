08f70f78536b4bb6e870056451b97ba513c1f2f1
Jon-Emil <jer8@hi.is>
2025-10-03 12:26:57 +0000

    fixed main

--- src/main/java/is/hi/hbv501g/hbv1/services/GenreService.java
+++ src/main/java/is/hi/hbv501g/hbv1/services/GenreService.java
@@ -3,9 +3,10 @@ import java.util.List;
 import is.hi.hbv501g.hbv1.persistence.entities.*;
 
 public interface GenreService {
-    List<Genre> getAllGenres();
-    List<Genre> getGenresByTitle(String title);
-    Genre createGenre(Genre genre);
-    Genre updateGenre(Genre genre, Long id);
-    void deleteGenre(Long id);
+    Genre findById(Long id);
+    List<Genre> findAll();
+    Genre findByTitle(String title);
+    Genre save(Genre game);
+    void delete(Genre game);
+
 }
