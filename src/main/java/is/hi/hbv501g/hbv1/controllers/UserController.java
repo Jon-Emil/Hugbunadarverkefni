365e1e5aa3ebc67d44abaa1e5eda1de1b04ff04a
Tryggvi <tryggvi2004@gmail.com>
2025-10-07 14:46:48 +0000

    Added a bunch of missing varibles for for review and setters and getters for them, also changed ResponseEntity method to include review and added necessery parameters for the usercontroller.

--- src/main/java/is/hi/hbv501g/hbv1/controllers/UserController.java
+++ src/main/java/is/hi/hbv501g/hbv1/controllers/UserController.java
@@ -1,9 +1,18 @@
 package is.hi.hbv501g.hbv1.controllers;
 
 import is.hi.hbv501g.hbv1.extras.PaginatedResponse;
+import is.hi.hbv501g.hbv1.persistence.entities.Game;
+import is.hi.hbv501g.hbv1.persistence.entities.Review;
 import is.hi.hbv501g.hbv1.persistence.entities.User;
+import is.hi.hbv501g.hbv1.persistence.repositories.GameRepository;
+import is.hi.hbv501g.hbv1.persistence.repositories.ReviewRepository;
+import is.hi.hbv501g.hbv1.persistence.repositories.UserRepository;
 import is.hi.hbv501g.hbv1.services.UserService;
+
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,10 +20,42 @@ import java.util.List;
 @RestController
 public class UserController {
     private final UserService userService;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+    
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService,UserRepository userRepository,
+                          GameRepository gameRepository,
+                          ReviewRepository reviewRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    @PostMapping("/{userId}/games/{gameId}/reviews")
+    public ResponseEntity<Review> postReview(
+            @PathVariable Long userId,
+            @PathVariable Long gameId,
+            @RequestBody Review incomingReview) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found: " + userId));
+
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Game not found: " + gameId));
+
+        Review review = new Review();
+        review.setRating(incomingReview.getRating());
+        review.setTitle(incomingReview.getTitle());
+        review.setText(incomingReview.getText());
+        review.setUser(user);
+        review.setGame(game);
+
+        Review saved = reviewRepository.save(review);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
@@ -37,4 +78,6 @@ public class UserController {
     ) {
         return userService.save(user);
     }
+
+
 }
--- src/main/java/is/hi/hbv501g/hbv1/persistence/entities/Review.java
+++ src/main/java/is/hi/hbv501g/hbv1/persistence/entities/Review.java
@@ -9,6 +9,13 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private int rating;
+
+     @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private String title;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
@@ -18,9 +25,12 @@ public class Review {
     public Review() {
     }
 
-    public Review(User user, Game game) {
+    public Review(User user, Game game, int rating, String text, String title) {
         this.user = user;
         this.game = game;
+        this.text = text;
+        this.title = title;
+        this.rating = rating;
     }
 
     public void setId(Long id) {
@@ -29,6 +39,36 @@ public class Review {
 
     public Long getId() {
         return id;
+    }
+
+     //get rating 
+    public int getRating() {
+        return rating;
+    }
+
+    //set rating
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    //get title
+    public String getTitle(){
+        return title;
+    }
+
+    //set title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // get text
+    public String getText() {
+        return text;
+    }
+
+    // set text
+    public void setText(String text) {
+        this.text = text;
     }
 
     public User getUser() {
