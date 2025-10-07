4cc2d769978358f65ed3e284028ac87c907c7a09
Tryggvi <tryggvi2004@gmail.com>
2025-10-07 15:44:53 +0000

    Fixed the ReviewRepository to be compatible with java spring

--- src/main/java/is/hi/hbv501g/hbv1/persistence/repositories/ReviewRepository.java
+++ src/main/java/is/hi/hbv501g/hbv1/persistence/repositories/ReviewRepository.java
@@ -1,9 +1,17 @@
 package is.hi.hbv501g.hbv1.persistence.repositories;
-import is.hi.hbv501g.hbv1.persistence.entities.*;
+
 import java.util.List;
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import is.hi.hbv501g.hbv1.persistence.entities.Game;
+import is.hi.hbv501g.hbv1.persistence.entities.Review;
+
+@Repository
+public interface ReviewRepository 
+    extends JpaRepository<Review, Long> {
     List<Review> findByGame(Game game);
     List<Review> findAll();
     Review save(Review review);
     void delete(Review review);
 }
+
